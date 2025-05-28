8a84ff393e58959f58369cbf3b22f909f0bd0138
aymenelmahboubi <elmahboubi.aymen2@gmail.com>
2025-05-28 10:43:19 +0100

    reservation+chefs

--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,86 @@ window.onclick = function(event) {
     modal.style.display = "none"
   }
 }
+
+
+
+// menu 
+
+
+let menutitles = document.querySelectorAll(".pageTitles .title")
+let menudivs = document.querySelectorAll(".ourMenu-body")
+
+menutitles.forEach((title) => {
+  title.addEventListener("click", () => {
+    menudivs.forEach((div) => {
+      div.classList.remove("active")
+      if (title.id === div.id) {
+        div.classList.add("active")
+      }
+    });
+
+    menutitles.forEach((ele) => {
+      ele.classList.remove("active")
+    });
+
+    title.classList.add("active")
+  });
+})
+
+
+
+// testem
+
+
+
+
+let containers = document.querySelectorAll(".carousel-container");
+let currentIndex = 0;
+
+const slideImage = (index, container) => {
+    let slides = container.querySelectorAll(".slide");
+    let camera = container.querySelector(".carousel");
+    let slideWidth = slides[0].clientWidth;
+    let indicators = container.querySelectorAll('.indicator');
+
+   
+    if (index < 0) {
+        index = slides.length - 1;
+    } else if (index >= slides.length) {
+        index = 0;
+    }
+
+    indicators.forEach(indicator => {
+        indicator.classList.remove('activeIndicator');
+    });
+    indicators[index].classList.add("activeIndicator");
+
+   
+    camera.style.transform = `translateX(-${slideWidth * index}px)`;
+    currentIndex = index;
+};
+
+
+containers.forEach(container => {
+ 
+    let slides = container.querySelectorAll(".slide")
+    let group = document.createElement("div")
+    group.classList.add("indicators-grp")
+    container.appendChild(group)
+
+    slides.forEach((slide, i) => {
+        let indicator = document.createElement("div")
+        indicator.classList.add("indicator")
+        group.appendChild(indicator)
+        
+      
+        indicator.addEventListener("click", () => {
+            slideImage(i, container);
+        })
+    })
+
+    indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator')
+
+    
+
+})
